0c7790c1015a0b2f732a671b08a9cbc0df301a41
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2022-07-13 18:37:36 +0900

    router change

--- movie/src/App.js
+++ movie/src/App.js
@@ -6,8 +6,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/movie/:id" element={<Detail />}></Route>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="/react_beginner/movie/:id" element={<Detail />}></Route>
+        <Route path="/react_beginner" element={<Home />}></Route>
       </Routes>
     </Router>
   );
